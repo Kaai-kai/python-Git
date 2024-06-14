78d904350a567b86c0c13ab432f5c35925980e67
Kaai-kai <c.chawakorn@outlook.com>
2024-06-14 16:46:49 +0700

    V2

---
+++ GitEp1.py
@@ -0,0 +1,4 @@
+def summation(num1,num2):
+    print("summation = ",(num1+num2))
+
+summation(5,10)
---
+++ GitEp2.py
@@ -0,0 +1,4 @@
+def subtraction(num1,num2):
+    print("Subtracktion = ",(num1-num2))
+
+subtraction(10,5)
